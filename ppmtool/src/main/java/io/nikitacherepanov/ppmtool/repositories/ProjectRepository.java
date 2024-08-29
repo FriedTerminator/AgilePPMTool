331c1bb65867096415576f5609dee43de7351ef8
Nikita Cherepanov <43394956+FriedTerminator@users.noreply.github.com>
2024-08-29 16:06:11 -0700

    added getters, setters, and new package

---
+++ ppmtool/src/main/java/io/nikitacherepanov/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,9 @@
+package io.nikitacherepanov.ppmtool.repositories;
+
+import io.nikitacherepanov.ppmtool.domain.Project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+}
