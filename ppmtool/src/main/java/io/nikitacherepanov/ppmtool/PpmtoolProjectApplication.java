2f1e96edd5993f259b8209faa3c565f71682bae6
Nikita Cherepanov <43394956+FriedTerminator@users.noreply.github.com>
2024-08-28 20:57:54 -0700

    first commit

---
+++ ppmtool/src/main/java/io/nikitacherepanov/ppmtool/PpmtoolProjectApplication.java
@@ -0,0 +1,13 @@
+package io.nikitacherepanov.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PpmtoolProjectApplication.class, args);
+    }
+
+}
---
+++ ppmtool/src/test/java/io/nikitacherepanov/ppmtool/PpmtoolProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nikitacherepanov.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
