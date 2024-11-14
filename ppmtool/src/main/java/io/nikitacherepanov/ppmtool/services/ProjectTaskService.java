f33e21d2422b52339d6f69d0cc85430f8cd96321
FriedTerminator <43394956+FriedTerminator@users.noreply.github.com>
2024-11-13 16:18:22 -0800

    able to add to back log after project task is created

---
+++ ppmtool/src/main/java/io/nikitacherepanov/ppmtool/services/ProjectTaskService.java
@@ -0,0 +1,48 @@
+package io.nikitacherepanov.ppmtool.services;
+
+import io.nikitacherepanov.ppmtool.domain.Backlog;
+import io.nikitacherepanov.ppmtool.domain.ProjectTask;
+import io.nikitacherepanov.ppmtool.repositories.BacklogRepository;
+import io.nikitacherepanov.ppmtool.repositories.ProjectTaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectTaskService {
+
+    @Autowired
+    private ProjectTaskRepository projectTaskRepository;
+
+    @Autowired
+    private BacklogRepository backlogRepository;
+
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
+
+        //Exceptions: Project Not Found
+
+        //PTs to be added to a specific project, project != null, BL exists
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+        //set the backlog to pt
+        projectTask.setBacklog(backlog);
+        //Project sequence will be like this: IDPRO-1   IDPRO-2 ...100 101
+        Integer BacklogSequence = backlog.getPTSequence();
+        //Update the BL Sequence
+        BacklogSequence++;
+
+        //Add sequence to project task
+        projectTask.setProjectSequence(projectIdentifier + "-" + BacklogSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
+
+        //Initial Priority when priority null
+//        if(projectTask.getPriority() == 0 || projectTask.getPriority() == null) {
+//            projectTask.setPriority(3);
+//        }
+
+        //Initial Status when status is null
+        if(projectTask.getStatus() == "" || projectTask.getStatus() == null) {
+            projectTask.setStatus("TO_DO");
+        }
+
+        return projectTaskRepository.save(projectTask);
+    }
+}
