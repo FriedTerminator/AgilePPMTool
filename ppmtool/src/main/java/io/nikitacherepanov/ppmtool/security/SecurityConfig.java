6468fd4a0627de957d1c9db52764ed71ac003959
FriedTerminator <43394956+FriedTerminator@users.noreply.github.com>
2025-01-12 12:39:22 -0800

    JWT Provider prework

--- ppmtool/src/main/java/io/nikitacherepanov/ppmtool/security/SecurityConfig.java
+++ ppmtool/src/main/java/io/nikitacherepanov/ppmtool/security/SecurityConfig.java
@@ -1,15 +1,20 @@
 package io.nikitacherepanov.ppmtool.security;
 
+import io.nikitacherepanov.ppmtool.services.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import static io.nikitacherepanov.ppmtool.security.SecurityConstants.*;
+import static io.nikitacherepanov.ppmtool.security.SecurityConstants.H2_URL;
+import static io.nikitacherepanov.ppmtool.security.SecurityConstants.SIGN_UP_URLS;
 
 @Configuration
 @EnableWebSecurity
@@ -21,7 +26,12 @@ import static io.nikitacherepanov.ppmtool.security.SecurityConstants.*;
 public class SecurityConfig {
 
     @Autowired
-    private JwtAuthenticationEntryPoint unauthorizedHandler = new JwtAuthenticationEntryPoint();
+    private JwtAuthenticationEntryPoint unauthorizedHandler;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
